91f010bf380c6edb6cf5ef7cf4a72ca75688f7cd
GhadeerHabeeb <ghadeerhabeeb2@gmail.com>
2022-12-02 17:32:51 +0300

    secondcommit

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:shose_application_3/Pages/Appbanner.dart';
 
 class MyProductsPage extends StatelessWidget {
@@ -19,6 +21,59 @@ class MyProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          color: Colors.blue,
+          width: MediaQuery.of(context).size.width,
+          height: 50,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 5.0,
+              vertical: 5,
+            ),
+            child: GNav(
+                color: Color(0xffE3F5FE),
+                // tab button ripple color when pressed
+                hoverColor:
+                    Color.fromARGB(255, 97, 97, 97), // tab button hover color
+                haptic: true, // haptic feedback
+                tabBorderRadius: 15,
+                tabActiveBorder: Border.all(
+                    color: Colors.black, width: 1), // tab button border
+                // tab button border
+                // tab button shadow
+                curve: Curves.easeOutExpo, // tab animation curves
+                duration: Duration(milliseconds: 900), // tab animation duration
+                gap: 8, // the tab button gap between icon and text
+                // unselected icon color
+                activeColor: Color.fromARGB(255, 0, 0, 0),
+                backgroundColor: Colors.blue, // selected icon and text color
+                iconSize: 24, // tab button icon size
+                tabBackgroundColor: Color(0xff2F62D7)
+                    .withOpacity(0.1), // selected tab background color
+                padding: EdgeInsets.symmetric(
+                    horizontal: 20, vertical: 5), // navigation bar padding
+                tabs: [
+                  GButton(
+                    icon: Icons.home,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: Icons.favorite_border_outlined,
+                    text: 'Likes',
+                  ),
+                  GButton(
+                    icon: Icons.search,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: Icons.person,
+                    text: 'Profile',
+                  )
+                ]),
+          ),
+        ),
+      ),
       body: Column(
         children: [
           Stack(children: [
@@ -133,6 +188,29 @@ class MyProductsPage extends StatelessWidget {
                   ),
                 ],
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Pick Shoe Size',
+                style: TextStyle(
+                    color: Color(0xff2F62D7),
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MySize(size: '4k6'),
+                  MySize(size: '5k7'),
+                  MySize(size: '6k8'),
+                  MySize(size: '7k9'),
+                ],
+              )
             ]),
           ))
         ],
@@ -186,7 +264,7 @@ class _MyshoesColorState extends State<MyshoesColor> {
           onTap: () {
             setState(() {
               _bordercolor == Colors.transparent
-                  ? _bordercolor = Color.fromARGB(255, 131, 131, 131)
+                  ? _bordercolor = Color.fromARGB(255, 19, 134, 23)
                   : _bordercolor = Colors.transparent;
             });
           },
@@ -204,6 +282,48 @@ class _MyshoesColorState extends State<MyshoesColor> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class MySize extends StatefulWidget {
+  final String size;
+  const MySize({required this.size, super.key});
+
+  @override
+  State<MySize> createState() => _MySizeState();
+}
+
+class _MySizeState extends State<MySize> {
+  Color _bordercolor = Colors.transparent;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _bordercolor == Colors.transparent
+              ? _bordercolor = Color.fromARGB(255, 19, 134, 23)
+              : _bordercolor = Colors.transparent;
+        });
+      },
+      child: Container(
+          height: 50,
+          width: 60,
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 183, 230, 253),
+              border: Border.all(
+                color: _bordercolor,
+                width: 4,
+              )),
+          child: Center(
+            child: Text(
+              widget.size,
+              style: TextStyle(
+                  color: Color(0xff2F62D7),
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+          )),
     );
   }
 }
